1a1344dec3b3839b5d905349efcb168d4d34b19f
CiaociaoStopiglia <stopiglia.design@gmail.com>
2025-09-29 09:16:09 -0300

    NEW 04-delete

---
+++ atividade-02-beauty/04-delete.sql
@@ -0,0 +1,23 @@
+DELETE FROM produtos
+WHERE estoque < 10;
+
+SELECT * FROM produtos;
+
+DELETE FROM produtos
+WHERE preco < 20;
+
+SELECT * FROM produtos;
+
+DELETE FROM produtos
+WHERE marca = 'Natura';
+
+SELECT * FROM produtos;
+
+DELETE FROM produtos
+WHERE estoque < 200;
+
+DELETE FROM produtos
+WHERE estoque > 25 AND estoque < 40;
+
+DELETE FROM produtos
+WHERE estoque BETWEEN 250 AND 400;
